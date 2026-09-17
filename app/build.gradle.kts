cb4bddaa033f269b3066f111947f830ff55863ab
GAIDI AMJED <150559809+Gautier7799@users.noreply.github.com>
2026-09-17 20:17:52 +0100

    Add lifecycle and saved state dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,5 +37,6 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.savedstate:savedstate-ktx:1.2.1")
 }
